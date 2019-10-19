2b9fbf57aaae77d257853f0b268302688a2b2b56
Ian Maguire <ian.maguire@u.group>
2019-10-18 17:53:50 -0700

    Adding a response to the entrypoint script...

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ -n "$KEY" -a -n "$MAC" ]; then
-	NAME=$(curl -v https://api.macaddress.io/v1?apiKey=$KEY&output=json&search=$MAC | grep "companyName" | awk -F\"\ '{print $2}')
+	RESPONSE=$(curl https://api.macaddress.io/v1?apiKey=$KEY&output=vendor&search=$MAC)
+	echo $RESPONSE
 else
 	echo "You must provide arguments for both KEY and MAC"
 fi
