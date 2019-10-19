7467cffceb82a059b5f034432c89f555957536b2
Ian Maguire <ian.maguire@u.group>
2019-10-18 17:59:19 -0700

    Addng quotes for curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "$KEY" -a -n "$MAC" ]; then
-	RESPONSE=$(curl https://api.macaddress.io/v1?apiKey=$KEY&output=vendor&search=$MAC)
+	RESPONSE=$(curl 'https://api.macaddress.io/v1?apiKey=$KEY&output=vendor&search=$MAC')
 	echo $RESPONSE
 else
 	echo "You must provide arguments for both KEY and MAC"
